37ce90c22907a7f1098c2b235d1380a791f80edd
Levi Omagbemi <123468114+leviomagbemi@users.noreply.github.com>
2023-03-21 21:36:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,129 +1,174 @@
-//UI variables
-const btnCon = document.getElementById('btncon');
-const input = document.querySelector('input');
-const anscon = document.querySelector('#anscon');
+const score = document.getElementById('score');
+const wordContainer = document.querySelector('.word-containers');
+const outputContainer = document.querySelector('.output-container');
+const output = document.getElementById('output');
+const letterContainer = document.querySelector('.letters-container');
+const congrats = document.querySelector('.congrats');
+const clap = document.querySelector('.clap');
+const continueBtn = document.getElementById('continue');
 
+//load letters on dcl
+document.addEventListener('DOMContentLoaded', loadLetters);
 
-// let score = 0;
+//data
+const data = [
+  {re: '^[NOSY]{3,4}$',
+   words:'NOSY,SON,SOY',
+   letters: 'NYSO'},
 
+   {re: '^[MUCH]{3,4}$',
+   words: 'MUCH,HUM,CHUM',
+   letters: 'MCHU'},
 
+   {re: '^[SPAN]{3,4}$',
+   words:'SPAN,PAN,NAP',
+   letters: 'NSAP'},
 
-//eventlistner
-btnCon.addEventListener('click', getBtnValue);
+   {re: '^[HOWL]{3,4}$',
+   words:'HOWL,LOW,OWL',
+   letters: 'LHWO'},
 
+   {re: '^[WIDE]{3,4}$',
+   words:'WIDE,DIE,DEW',
+   letters: 'IEWD'},
+];
 
-//regular expression
-let re = /^[NOSY]{3,4}$/;
+//iterator
+function wordIterator(words){
+  let wordIndex = 0;
 
+  return {
+    next: function(){
+      return (wordIndex < words.length) ?
+      {value: words[wordIndex++], done: false} :
+      {done: true}
+    }
+  }
+};
 
-const str =['NOSY', 'SON', 'SOY'];
+//iterate through data
+let wordArr = wordIterator(data);
 
-letters = str[0].split("").reverse();
+//get iterator value
+let currentWord = wordArr.next().value;
 
-document.getElementById('score').textContent
+//set iterator values
+let re = new RegExp(currentWord.re);
+let words = currentWord.words.split(',');
+let cw = []
 
-//match word with re
-function getWords(){
- 
-  str.forEach(word => {
-    if(input.value.length >= 3 && word.match(re) != input.value){
-      input.classList.add('bg-danger', 'text-light');
+//loadletter fun
+function loadLetters(){
+ let letters = currentWord.letters.split('').reverse();
+ letters.forEach(letter => {
+  letterContainer.innerHTML += `<span class= "letter">${letter}</span>`;
+ })
+};
+
+//addeventlistener to buttons
+letterContainer.addEventListener('click', displayletters);
+continueBtn.addEventListener('click', continueGame)
+
+//display letters
+function displayletters(e){
+  if(e.target.className === 'letter'){
+    output.style.display = 'block';
+    output.value += e.target.textContent;
+  }
+  
+  checkExistingWord();
+};
+
+//check if word is correct
+function checkWords(){
+ //turn words to array
+  words = currentWord.words.split(',');
+
+  //loop through words
+  words.forEach(word => {
+    if(String(word.match(re)) === output.value){
+     wordContainer.innerHTML += `<span class="word">${word}</span>`;
+     output.classList.add('correct');
+
+     setTimeout(() => {
+      output.classList.remove('correct');
+     }, 1000)
+      
+     celebration();
+        
+    score.textContent++
+
+      cw.push(word)
+    } else if
+
+    (output.value !== String(word.match(re))){
+      output.classList.add('wrong');
 
       setTimeout(() => {
-        input.classList.remove('bg-danger', 'text-light');
-        input.value = '';
-      }, 1000)
-
-  } else if(word.match(re) == input.value) {
-    input.classList.remove('bg-danger');
-
-      //initialize span  
-      let span = document.createElement('span');  
-
-       span.className = 'btn btn-success mx-1 my-1';
-
-       span.id = 'spantext'
-
-      span.appendChild(document.createTextNode(word))
-        
-  
-      //append span
-      anscon.appendChild(span)
-
-      //clear input
-      input.value = '';
-      
-      document.getElementById('score').textContent++
-
-      // gettext(document.querySelectorAll('#spantext'))
-      
-  }});
-  };
-
-  
-
- 
-
-//loop through letters
-document.addEventListener('DOMContentLoaded', () => {
-
-
-  let span = '';
-
-  letters.forEach(letter => {
-  span += `<span class="btn btn-primary mx-1" id="btnLet">${letter}</span>`;
+       output.classList.remove('wrong');
+       output.value = '';
+       output.style.display = 'none';
+      }, 1000);
+      }  
   });
+
+  displayContinueBtn();
+};
+
+function displayContinueBtn(){
+//check if correct words equal to 3 and display continue btn 
+if(wordContainer.childElementCount === 3){
+ continueBtn.style.display = 'block'
+   }
+};
+
+function celebration(){
+if(wordContainer.childElementCount === 3){
+  output.style.display = 'none'
+  congrats.style.display = 'block'
+
+  setTimeout(() => {
+    congrats.style.display = 'none'
+  }, 2000)}
   
-  btnCon.innerHTML = span;
+   else {
+  clap.style.display = 'block'
 
-});
+  setTimeout(() => {
+    clap.style.display = 'none'
+  }, 2000)}};
 
-function getBtnValue(e){
-  if(e.target.className === 'btn btn-primary mx-1'){
-    input.value += e.target.textContent;
-  };
+//check for existing word
+function checkExistingWord(){
+  if(output.value.length >= 3 && !cw.includes(output.value)){
+     checkWords();
+  } else if(output.value.length >= 3 && cw.includes(output.value)) {
+    output.classList.add('exist')
+    output.value = 'Word already exist'; 
 
-  if(input.value == gettext(document.querySelectorAll('#spantext'))){
-    alert('Word already exist');
-    input.value = '';
-  } else {
-    getWords();
- } 
- congrats(document.querySelectorAll('#spantext'))
-}
+    setTimeout(() => {
+      output.classList.remove('exist')
+      output.value = ''; 
+      output.style.display = 'none';
+    }, 1000)
+  }};
+
+//continue game
+function continueGame(){
+  continueBtn.style.display = 'none'
+
+  //next word 
+ currentWord = wordArr.next().value;
  
-
-function gettext(spantext){
-  let text = '';
-
-  spantext.forEach((tex) => {
-  text = tex.textContent;
-  })
-
-return text;
-}
-
-function congrats(spanlength){
-   if(spanlength.length == 3){
-  alert('congratulations')
- }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+ //clear word and letter container
+ letterContainer.innerHTML = '';
+ wordContainer.innerHTML = '';   
+ 
+ //set reg and word to current value
+ re = new RegExp(currentWord.re);
+ words = currentWord.words.split(',');
+ 
+ //call functions
+ loadLetters();
+ checkWords();
+};
